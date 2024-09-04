06a46c4e3bb3b8f51457f42dfcc2d9591c999a7e
SamNdungu <samwainaina087@gmail.com>
2024-09-04 22:13:33 +0300

    Edited cards3.py file

--- CS50P/WK4:Libraries/Shorts_random/cards3.py
+++ CS50P/WK4:Libraries/Shorts_random/cards3.py
@@ -4,7 +4,15 @@ cards = ["jack", "queen", "king"]
 
 
 def main():
+
+    # 'Jack' has a 100% chance to be chosen - user will always choose 'Jack'
     print(random.choices(cards, weights=[100, 0, 0], k=2))
 
+    # 'Jack', 'queen', and 'king' has 65%, 20%, and 15% chance to be chosen respectively
+    print(random.choices(cards, weights=[65, 20, 15], k=2))
+
+    # 'random.seed(x)' makes the sure you get the same results
+    random.seed(1)
+    print(random.choices(cards, weights=[65, 20, 15], k=2))
 
 main()
