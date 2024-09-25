48bcfed3eea99e37f9bf365c2fc7f20e4b3a79b2
SamNdungu <samwainaina087@gmail.com>
2024-09-25 22:05:21 +0300

    Added a comment on number6.py file

--- CS50P/WK3:Exceptions/number6.py
+++ CS50P/WK3:Exceptions/number6.py
@@ -4,6 +4,7 @@ def main():
     x = get_int()
     print(f"x is {x}")
 
+
 # Define a function get_int()
 def get_int():
     while True:
@@ -12,7 +13,7 @@ def get_int():
         except ValueError:
             print("x is not an integer")
         else:
-            # `return x` is better than `break`, it will not only break you out of the loop but it will also return a value.
+            # `return x` is better than `break`. it will not only break you out of the loop but it will also return a value.
             return x
 
 
